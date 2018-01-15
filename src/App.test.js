584b15fe76e7c05c3b578ac28eba8ebcc4a4f2ed
Douglas Wissett Walker <douglas.wissett@gmail.com>
2018-01-15 16:36:30 +0000

    written tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MapWithAMarker from './MapWithAMarker';
+import fetch from 'isomorphic-fetch'
+
+let url = 'https://nameless-castle-51857.herokuapp.com/api/v1/stashpoints';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders map without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<MapWithAMarker
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRQJfPMLOKMiegBvbLsBlgKqIA8J_pinA&v=3.exp&libraries=geometry,drawing,places"
+              loadingElement={<div style={{ height: `70vh` }} />}
+              containerElement={<div style={{ height: `400px` }} />}
+              mapElement={<div style={{ height: `70vh` }} />} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+// test API
+// it('should load api data', () => {
+//   return fetch(url)
+//   .then(data => {
+//     expect(data).toBeDefined()
+//     expect(data.entity.name).toEqual('Koen van Gilst')
+//   })
+// });
